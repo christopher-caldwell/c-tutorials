a11d397485e9bc41764e880295efee47625a3109
Christopher Caldwell <1127514@iheartmedia.com>
2020-12-09 08:58:47 -0500

    trimming down build script

--- scripts/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gcc -g src/main.c -o dist/main
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-pwd
-sh ./scripts/compile.sh
+SRC_PATH=${1:-src/main}
+OUTPUT_PATH=${2:-dist/main}
+
+echo $SRC_PATH
+echo $OUTPUT_PATH
+
+gcc -g $SRC_PATH -o $OUTPUT_PATH
 
 if [ $? != 0 ]; then
   exit 1
 fi
 
-sh ./scripts/run.sh
-
-if [ $? != 0 ]; then
-  exit 1
-fi
+$OUTPUT_PATH
